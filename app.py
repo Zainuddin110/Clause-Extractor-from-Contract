102bb814193641243f85ec71fc0848da4a791d50
Zainuddin110 <139258194+Zainuddin110@users.noreply.github.com>
2025-09-25 17:31:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import spacy
+import os
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 import os
 import streamlit as st
 import joblib
@@ -75,5 +84,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
